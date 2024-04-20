4fc69748711991908290fba5f0557ccb3d0ec209
Robin Nilsson <robin.nilsson@ist.com>
2024-04-20 23:12:34 +0200

    setup tailwindcss and default structure

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data } = await useFetch("http://semester-api.test/api/absences");
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <div>data: {{ data }}</div>
+  </div>
+</template>
