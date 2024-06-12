de6edef9a777890b4972314ad313487f4c85b515
Robin Nilsson <robin.nilsson@ist.com>
2024-06-12 20:49:29 +0200

    ring colors on radio button

--- pages/absences/new.vue
+++ pages/absences/new.vue
@@ -109,7 +109,10 @@ watchEffect(() => {
           class="flex p-3 gap-x-3 items-center"
         >
           <input
-            class="radio-button dark:focus:border-accent-dark dark:hover:border-accent-dark dark:focus:ring-0 dark:focus:ring-offset-transparent dark:focus:ring-accent-dark ring-2 dark:border-accent-dark dark:checked:after:bg-accent-dark dark:bg-neutral-900 bg-white dark:ring-accent-dark dark:text-accent-dark text-accent-light dark:checked:bg-transparent checked:bg-accent-light focus:border-accent-light hover:border-accent-light focus:ring-accent-light border-accent-light checked:after:bg-accent-light ring-accent-light"
+            :class="[
+              'radio-button dark:focus:border-accent-dark dark:checked:ring-offset-accent-dark dark:hover:border-accent-dark dark:focus:ring-offset-accent-dark dark:focus:ring-accent-dark ring-2 dark:border-accent-dark dark:checked:after:bg-accent-dark dark:bg-neutral-900 bg-white dark:ring-accent-dark dark:text-accent-dark dark:checked:bg-transparent',
+              'checked:focus:ring-offset-transparent focus:ring-offset-black checked:bg-white checked:ring-offset-accent-light focus:ring-black checked:focus:ring-accent-light ring-offset-black dark:ring-offset-accent-dark text-accent-light border-0 ring-offset-2 focus:checked:bg-white focus:border-accent-light hover:border-black hover:checked:bg-white checked:ring-accent-light border-black checked:after:bg-accent-light ring-black',
+            ]"
             type="radio"
             :id="absenceType.id"
             :value="absenceType.id"
