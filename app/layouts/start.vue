c16d9e54bd691a17fc4ca0b3db7a232360f79db2
Jonas Erlandsson <jonas.erlandson@ist.com>
2024-07-18 14:42:47 +0200

    fix dark mode on login page

--- app/layouts/start.vue
+++ app/layouts/start.vue
@@ -6,7 +6,7 @@ const { currentPage } = useCurrentPage();
 
 <template>
   <div
-    class="bg-primary w-screen h-auto min-h-screen text-white flex flex-col gap-y-4"
+    class="bg-primary w-screen h-auto min-h-screen dark:bg-primary-dark dark:text-white flex flex-col gap-y-4"
     :class="
       currentPage === 'login'
         ? 'items-center py-2'
@@ -15,7 +15,9 @@ const { currentPage } = useCurrentPage();
   >
     <div class="flex gap-x-2">
       <h1 class="text-2xl">Semesterappen</h1>
-      <span class="size-5 self-center text-yellow-200 material-icons">favorite</span>
+      <span class="size-5 self-center text-yellow-200 material-icons"
+        >favorite</span
+      >
     </div>
     <slot />
   </div>
