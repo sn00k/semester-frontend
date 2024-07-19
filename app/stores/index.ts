7bcebdb4762a7dd7b30d672c7f0ef6c09c250bf0
Robin Nilsson <robin.nilsson@ist.com>
2024-07-19 23:04:10 +0200

    add absence composable and various bugfixes for my time

---
+++ app/stores/index.ts
@@ -0,0 +1,15 @@
+import { useAbsenceStore } from './absenceStore';
+import { useAuthStore } from './authStore';
+import { useMenuStore } from './menuStore';
+import { useNotificationStore } from './notificationStore';
+import { useSettingsStore } from './settingsStore';
+import { useUserStore } from './userStore';
+
+export {
+  useAbsenceStore,
+  useAuthStore,
+  useMenuStore,
+  useNotificationStore,
+  useSettingsStore,
+  useUserStore,
+};
